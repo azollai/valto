7beadd9f30180e6894c05aba334b976d7ac4bb09
Andras Zollai <andrszollai@gmail.com>
2018-04-29 19:30:03 +0300

    Building layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,20 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'valto'}),
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
+    BrowserModule.withServerTransition({ appId: 'valto' }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
