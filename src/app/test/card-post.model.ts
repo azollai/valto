a0990e92fc38cc08c03ebe39899b58c09b6de0d5
Andras Zollai <andrszollai@gmail.com>
2018-08-06 17:15:52 +0200

    Generic feed implementation

--- src/app/test/card-post.model.ts
+++
@@ -1,8 +0,0 @@
-export class CardPostModel {
-  upVote: number;
-  downVote: number;
-  place: string;
-  tags: string[];
-  urls: string[];
-  description: string;
-}
--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -1,23 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PlaceSearchModule } from '../common/place-search/place-search.module';
-import {
-  MatButtonModule,
-  MatButtonToggleModule, MatCardModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule, MatStepperModule
-} from '@angular/material';
 import { TestComponent } from './test.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestRoutingModule } from './test-routing.module';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { SlideshowModule } from 'ng-simple-slideshow';
-import { ImageCropperModule } from '../common/image-wrapper/image-cropper/image-cropper.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { CardPostModule } from '../common/card-post/card-post.module';
+import { FeedService } from './feed.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ImageWrapperModule } from '../common/image-wrapper/image-wrapper.module';
-import { ChipsSelectModule } from '../common/chips-select/chips-select.module';
-import { FormControlErrorMessagesModule } from '../common/form-control-error-messages/form-control-error-messages.module';
 
 
 @NgModule({
@@ -26,28 +14,11 @@ import { FormControlErrorMessagesModule } from '../common/form-control-error-mes
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-
-    PlaceSearchModule,
-
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatStepperModule,
-
     TestRoutingModule,
-
-    AngularFireStorageModule,
-    ImageWrapperModule,
-
-    MatCardModule,
-    ChipsSelectModule,
-    FormControlErrorMessagesModule
+    FlexLayoutModule,
+    InfiniteScrollModule,
+    CardPostModule,
   ],
-  providers: []
+  providers: [FeedService]
 })
 export class TestModule {}
