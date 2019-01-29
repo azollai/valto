4b332196c8b284e9ab09f13e1310c6f4621a8082
Andras Zollai <andrszollai@gmail.com>
2019-01-29 22:38:01 +0100

    karma config

--- karma.conf.js
+++ karma.conf.js
@@ -6,11 +6,11 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require( 'karma-jasmine' ),
+      require( 'karma-chrome-launcher' ),
+      require( 'karma-phantomjs-launcher' ),
+      require( 'karma-remap-istanbul' ),
+      require( 'angular-cli/plugins/karma' )
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
